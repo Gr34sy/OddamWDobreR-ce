52321d2331eea32105dfc82e8373a93751f8e636
Gr34sy <slupek3@poczta.fm>
2022-11-16 17:36:37 +0100

    added help section responsiveness

--- src/components/Help.js
+++ src/components/Help.js
@@ -9,7 +9,40 @@ export function Help(){
             <figure className="help__figure">
                 <img src={decoration} alt="text decoration" />
             </figure>
+
             <Tabs/>
+
+            <div className="help__container">
+                <div className="help__col">
+                    <div className="col__circle">
+                        Fundacjom
+                    </div>
+
+                    <p className="col__description">
+                        W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
+                    </p>
+                </div>
+
+                <div className="help__col">
+                    <div className="col__circle">
+                        ORGANIZACJOM POZARZĄDOWYM
+                    </div>
+
+                    <p className="col__description">
+                        Pomagamy również wszelkim organizacjom pozarządowym i charytatywnym, które nie są Fundacjami. Są to nasi Partnerzy, który zrobią dobry pożytek z rzeczy, które do nich trafią.
+                    </p>
+                </div>
+
+                <div className="help__col">
+                    <div className="col__circle">
+                        Lokalnym zbiórkom
+                    </div>
+
+                    <p className="col__description">
+                        W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.
+                    </p>
+                </div>
+            </div>
         </section>
     )
 }
