dbea10f3eca3600d6964f40bf639d7353d60fb7b
Gr34sy <slupek3@poczta.fm>
2022-11-29 16:04:14 +0100

    added step 3 and 4 styling in give form

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -9,11 +9,14 @@ export function Stepper(){
     //bags state
     const [showBags, setShowBags] = useState(false)
 
+    //location state
+    const [showLocation, setShowLocation] = useState(false)
+
     function stepForward(e){
         e.preventDefault();
         setCurrentStep(
             prevStep=>{
-                if(prevStep<4){
+                if(prevStep<5){
                     return prevStep + 1;
                 }else{
                     return prevStep;
@@ -45,6 +48,16 @@ export function Stepper(){
         }
     }
 
+    function toggleLocation(e){
+        e.preventDefault();
+
+        if(showLocation===true){
+            setShowLocation(false)
+        }else{
+            setShowLocation(true)
+        }
+    }
+
     return(
         <section className="stepper">
             { currentStep<=4 &&
@@ -58,7 +71,10 @@ export function Stepper(){
             }
 
             <div className="stepper__container">
-                <p className="stepper__current-step">Krok {currentStep}/4</p>
+                {currentStep<=4 &&
+                <p className="stepper__current-step">
+                    Krok {currentStep}/4
+                </p>}
 
                 <form className="stepper__form">
                     {currentStep===1 && 
@@ -109,7 +125,7 @@ export function Stepper(){
                                 </div>
                             </div>
 
-                            { showBags &&<div className='bags'>
+                            { showBags &&<div className='custom-options options--bags'>
                                 <p>1</p>
                                 <p>2</p>
                                 <p>3</p>
@@ -124,20 +140,110 @@ export function Stepper(){
 
                     {currentStep===3 && 
                     <div className="stepper__step step--3">
-                        <input type="radio"/>
-                    </div>
-                    }
+                        <h3 className="stepper-form__title">
+                           Lokalizacja:
+                        </h3>
+
+                        <div className="custom-select__box">
+                            <div className="custom-select" onClick={toggleLocation}>
+                                <div>
+                                    <p>wybierz</p>
+                                    <img src={ showLocation===true ? arrowUp : arrowDown} alt="arrow down"/>
+                                </div>
+                            </div>
+
+                            { showLocation &&<div className='custom-options options--locations'>
+                                <p>Poznań</p>
+                                <p>Warszawa</p>
+                                <p>Kraków</p>
+                                <p>Wrocław</p>
+                                <p>Katowice</p>
+                                <p>Łódź</p>
+                            </div>}
+                        </div>
+
+                        <p className='step__description'>
+                            Komu chcesz pomóc?
+                        </p>
+                        <div className='step-help__boxes'>
+                            <input type="button" className='button button--small' value="dzieciom"/>
+                            <input type="button" className='button button--small' value="samotnym matkom"/>
+                            <input type="button" className='button button--small' value="bezdomnym"/>
+                            <input type="button" className='button button--small' value="niepełnosprawnym"/>
+                            <input type="button" className='button button--small' value="osobom starszym"/>
+                        </div>
+
+                        <p className='step__description'>
+                            Wpisz nazwę konkretnej organizacji (opcjonalnie)
+                        </p>
+                        <input type="text" className='stepper__custom-input custom-input--org' />
+                    </div>}
                     
                     {currentStep===4 && 
                     <div className="stepper__step step--4">
-                        <input type="checkbox"/>
+                        <h3 className="stepper-form__title">
+                            Podaj adres oraz termin odbioru rzecz przez kuriera
+                        </h3>
+
+                        <div className='step__last-box'>
+                            <div className='step__address'>
+                                <p className='step__description'>
+                                    Adres odbioru:
+                                </p>
+
+                                <label htmlFor="street">
+                                    Ulica
+                                    <input type="text" name="street" className="stepper__custom-input input--final-box" />
+                                </label>
+
+                                <label htmlFor="city">
+                                    Miasto
+                                    <input type="text" name="city" className="stepper__custom-input input--final-box" />
+                                </label>
+
+                                <label htmlFor="postcode">
+                                    Kod<br/>
+                                    pocztowy
+                                    <input type="text" name="postcode" className="stepper__custom-input input--final-box" />
+                                </label>
+
+                                <label htmlFor="phone">
+                                    Numer<br/>
+                                    telefonu
+                                    <input type="text" name="phone" className="stepper__custom-input input--final-box" />
+                                </label>
+                            </div>
+
+                            <div className='step__term'>
+                                <p className='step__description'>
+                                    Termin odbioru:
+                                </p>
+
+                                <label htmlFor="date">
+                                    Data
+                                    <input type="text" name="date" className="stepper__custom-input input--final-box" />
+                                </label>
+
+                                <label htmlFor="hour">
+                                    Godzina
+                                    <input type="text" name="hour" className="stepper__custom-input input--final-box" />
+                                </label>
+
+                                <label htmlFor="notes">
+                                    Uwagi <br/> 
+                                    dla kuriera
+                                    <input type="text" name="notes" className="stepper__custom-input input--final-box" />
+                                </label>
+                            </div>
+                        </div>
                     </div>
                     }
                 </form>
 
                 <div className="stepper__buttons">
                     { currentStep >1 && <input type="button"  className="button button--middle button--stepper" value="Wstecz" onClick={stepBackward}/>}
-                    {  currentStep <4 && <input type="button" className="button button--middle button--stepper"  value="Dalej" onClick={stepForward}/>}
+                    {  currentStep <5 && <input type="button" className="button button--middle button--stepper"  value="Dalej" onClick={stepForward}/>}
+                    {  currentStep===5 && <input type="submit" className="button button--middle button--stepper"  value="Potwierdzam" />}
                 </div>
             </div>
         </section>
