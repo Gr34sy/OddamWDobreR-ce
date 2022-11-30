49aeeb1c6c18a3a11453e6552220f18985eca0c2
Gr34sy <slupek3@poczta.fm>
2022-11-30 12:57:44 +0100

    added input handling to steps 1 and 2

--- src/components/Stepper.js
+++ src/components/Stepper.js
@@ -14,6 +14,37 @@ export function Stepper(){
     //location state
     const [showLocation, setShowLocation] = useState(false)
 
+    //form states
+    const INITIAL_CHECKBOXES = {
+        reusableClothes: false,
+        rubbishClothes: false,
+        toys: false,
+        books: false,
+        others: false
+    }
+    const [checkboxes, setCheckboxes] = useState(INITIAL_CHECKBOXES)
+
+    const [bagAmount, setBagAmount] = useState('wybierz')
+
+    function handleCheckbox(e){
+        if(e.target.checked){
+            setCheckboxes({
+                ...checkboxes,
+                [e.target.name]: true,
+            })
+        }else{
+            setCheckboxes({
+                ...checkboxes,
+                [e.target.name]: false,
+            })
+        }
+    }
+
+    function handleBags(e){
+        e.preventDefault();
+        setBagAmount(e.target.getAttribute("data-value"));
+    }
+
     function stepForward(e){
         e.preventDefault();
         setCurrentStep(
@@ -64,11 +95,11 @@ export function Stepper(){
         <section className="stepper">
             { currentStep<=4 &&
                 <h2 className="stepper__header">
-                    Ważne!<br/>
-                    {currentStep===1 && "Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać."}
-                    {currentStep===2 && "Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ."}
-                    {currentStep===3 && "Jeśli wiesz komu chcesz pomóc, możesz wpisać nazwę tej organizacji w wyszukiwarce. Możesz też filtrować organizacje po ich lokalizacji bądź celu ich pomocy."}
-                    {currentStep===4 && "Podaj adres oraz termin odbioru rzeczy."}
+                    <span>Ważne!</span>
+                    {currentStep===1 && <p>"Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać."</p>}
+                    {currentStep===2 && <p>"Wszystkie rzeczy do oddania zapakuj w 60l worki. Dokładną instrukcję jak poprawnie spakować rzeczy znajdziesz TUTAJ."</p>}
+                    {currentStep===3 && <p>"Jeśli wiesz komu chcesz pomóc, możesz wpisać nazwę tej organizacji w wyszukiwarce. Możesz też filtrować organizacje po ich lokalizacji bądź celu ich pomocy."</p>}
+                    {currentStep===4 && <p>"Podaj adres oraz termin odbioru rzeczy."</p>}
                 </h2>
             }
 
@@ -85,28 +116,28 @@ export function Stepper(){
                             Zaznacz co chcesz oddać:
                         </h3>
 
-                        <label htmlFor="reusable-clothes" className="label--checkbox">
-                            <input type="checkbox" name="reusable-clothes" className="custom-checkbox" value="reusableClothes"/>
+                        <label htmlFor="reusableClothes" className="label--checkbox">
+                            <input type="checkbox" name="reusableClothes" className="custom-checkbox"  checked={checkboxes.reusableClothes} onChange={handleCheckbox}/>
                             ubrania, które nadają się do ponownego użycia
                         </label>
 
-                        <label htmlFor="rubbish-clothes" className="label--checkbox">
-                            <input type="checkbox" name="rubbish-clothes" className="custom-checkbox" value="rubbishClothes"/>
+                        <label htmlFor="rubbishClothes"className="label--checkbox">
+                            <input type="checkbox" name="rubbishClothes" className="custom-checkbox" checked={checkboxes.rubbishClothes} onChange={handleCheckbox}/>
                             ubrania, do wyrzucenia
                         </label>
 
                         <label htmlFor="toys" className="label--checkbox">
-                            <input type="checkbox" name="toys" className="custom-checkbox" value="toys"/>
+                            <input type="checkbox" name="toys" className="custom-checkbox" checked={checkboxes.toys} onChange={handleCheckbox}/>
                             zabawki
                         </label>
 
                         <label htmlFor="books" className="label--checkbox">
-                            <input type="checkbox" name="books" className="custom-checkbox" value="books"/>
+                            <input type="checkbox" name="books" className="custom-checkbox"  checked={checkboxes.books} onChange={handleCheckbox}/>
                             książki
                         </label>
 
                         <label htmlFor="others" className="label--checkbox">
-                            <input type="checkbox" name="others" className="custom-checkbox" value="others" />
+                            <input type="checkbox" name="others" className="custom-checkbox" checked={checkboxes.others} onChange={handleCheckbox}/>
                             inne
                         </label>
                     </div>
@@ -122,18 +153,18 @@ export function Stepper(){
                             <div className="custom-select" onClick={toggleBags}>
                             Liczba 60l worków:
                                 <div>
-                                    <p>wybierz</p>
+                                    <p>{bagAmount}</p>
                                     <img src={ showBags===true ? arrowUp : arrowDown} alt="arrow down"/>
                                 </div>
                             </div>
 
                             { showBags &&<div className='custom-options options--bags'>
-                                <p>1</p>
-                                <p>2</p>
-                                <p>3</p>
-                                <p>4</p>
-                                <p>5</p>
-                                <p>6</p>
+                                <p data-value="1" onClick={handleBags}>1</p> 
+                                <p data-value="2" onClick={handleBags}>2</p>
+                                <p data-value="3" onClick={handleBags}>3</p>
+                                <p data-value="4" onClick={handleBags}>4</p>
+                                <p data-value="5" onClick={handleBags}>5</p>
+                                <p data-value="6" onClick={handleBags}>6</p>
                             </div>}
 
                         </div>
